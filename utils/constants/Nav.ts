132e0af9a8095dc8152781ea8d698bf8ebf4bab7
isaackd <isaackd331@gmail.com>
2025-02-21 08:59:49 +0900

    feat : Header 메뉴 인터페이스 선언 및 메뉴 상수 선언

---
+++ utils/constants/Nav.ts
@@ -0,0 +1,28 @@
+import { INav } from "@/interface/header/interface";
+
+export const Nav: INav[] = [
+  {
+    name: "차트",
+    value: "chart",
+  },
+  {
+    name: "Whook",
+    value: "whook",
+  },
+  {
+    name: "이벤트",
+    value: "event",
+  },
+  {
+    name: "뉴스",
+    value: "news",
+  },
+  {
+    name: "스토어",
+    value: "store",
+  },
+  {
+    name: "충전소",
+    value: "charge",
+  },
+];
