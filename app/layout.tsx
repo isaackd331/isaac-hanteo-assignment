21815aa5a81253aa262582a625e13c9eb43a422f
isaackd <isaackd331@gmail.com>
2025-02-21 10:13:42 +0900

    feat : body min-height 속성 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body className="relative">
+      <body className="relative min-h-[100vh]">
         <MswProvider>
           <QueryProvider>
             <Header />
